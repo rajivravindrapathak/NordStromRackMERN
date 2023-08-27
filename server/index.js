01a58806b2ae3b706f4e15c7c39a0a60326c2b45
Rajivpathak <rajivpathak199@gmail.com>
2023-08-27 20:44:56 +0530

    user api signup login api created and connected to database

--- server/index.js
+++ server/index.js
@@ -1,1 +1,33 @@
+const express = require("express")
+const cors = require("cors")
 
+const {userController} = require("./routes/user.routes")
+
+const { connection } = require("./config/db")
+require('dotenv').config()
+
+
+const app = express()
+const PORT = process.env.PORT || 4000
+
+app.use(express.json())  
+app.use(cors()); 
+
+app.get("/", (req, res) => {
+    res.send("home page")
+})
+
+app.use(cors())
+
+app.use("/", userController)
+
+app.listen(PORT, async ()=> {    
+    try {
+        await connection;
+        console.log("connected to DB")
+    } catch (err) {
+        console.log("not connected to db")    
+        console.log(err)
+    }
+    console.log(`listning on PORT ${PORT}`)
+})
---
+++ server/routes/user.routes.js
@@ -0,0 +1,62 @@
+const { Router } = require("express")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+
+const {UserModel} = require("../models/User.model") 
+
+const userController = Router()
+
+// signup api to send user data in database
+userController.post("/signup", async (req, res) => {
+    const { email, password, age } = req.body
+
+    const isUser = await UserModel.findOne({ email })
+    if(isUser) {
+        res.send({ "msg" : "user already exits please login", isUser })
+    } else {
+        bcrypt.hash(password, 5, async function(err, hash) {
+            if(err) {
+                res.send({ msg: "sometning went wrong, plz try again later", err })
+            } 
+            else {
+                const user = new UserModel({    
+                    email,
+                    password: hash,
+                    age
+                })
+                try {          
+                    await user.save()
+                    res.status(200).send({ msg: "signUp Successful", user })
+                } catch (err) {
+                    console.log(err)
+                    res.send({ msg: "something went wrong, plz try again", err })
+                }
+            }
+
+        });
+    }   
+  
+})     
+
+// login api 
+userController.post("/login", async (req, res) => {
+    debugger
+    const { email, password } = req.body
+   
+    const user = await UserModel.findOne({email})
+    const hash = user.password
+    bcrypt.compare(password, hash, function(err, result) {
+        if(err) {
+            res.status(500).send({ msg: "sometning went wrong, plz try again later", err })
+        }
+        if(result) {
+            const token = jwt.sign({ userId: user._id}, process.env.JWT_SECRET)
+            res.json({ msg: "Login successful", token, userId: user._id })
+        } else {
+            res.status(401).send({ msg: "Invalid crdential" })
+        }
+    });
+})
+
+module.exports = { userController }
